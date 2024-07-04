1bfb4adda9f610a8bbb8fa263a4be19b56e9d024
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2024-07-04 15:01:44 -0400

    add compression script for images

--- src/utils/compressImages.js
+++ src/utils/compressImages.js
@@ -1,8 +1,8 @@
 import sharp from "sharp";
 import fs from "fs";
 import path from "path";
-import { promisify } from "util";
-import { glob } from "glob";
+
+console.log("Starting the compression script...");
 
 // Define the source and destination directories
 const srcDir = path.join("public/photography");
@@ -11,28 +11,34 @@ const destDir = path.join("public/photography-compressed");
 // Create the destination directory and any necessary subdirectories if they don't exist
 const ensureDirExists = (dir) => {
   if (!fs.existsSync(dir)) {
+    console.log(`Creating directory: ${dir}`);
     fs.mkdirSync(dir, { recursive: true });
+  } else {
+    console.log(`Directory already exists: ${dir}`);
   }
 };
 
 ensureDirExists(destDir);
 
-const globPromise = promisify(glob);
-
 (async () => {
   try {
-    const files = await globPromise(`${srcDir}/*.{jpg,jpeg,png}`);
+    console.log("Reading files...");
+    const files = fs
+      .readdirSync(srcDir)
+      .filter((file) => /\.(jpg|jpeg|png)$/.test(file));
+    console.log(`Found ${files.length} files`);
 
     await Promise.all(
       files.map(async (file) => {
-        const fileName = path.basename(file);
+        const filePath = path.join(srcDir, file);
         const outputFilePath = path.join(
           destDir,
-          fileName.replace(/\.(jpg|jpeg|png)$/, ".webp")
+          file.replace(/\.(jpg|jpeg|png)$/, ".webp")
         );
 
         try {
-          await sharp(file)
+          console.log(`Processing file: ${filePath}`);
+          await sharp(filePath)
             .resize(800, 600, {
               fit: sharp.fit.inside,
               withoutEnlargement: true,
@@ -41,9 +47,11 @@ const globPromise = promisify(glob);
             .webp({ quality: 80 })
             .toFile(outputFilePath);
 
-          console.log(`Compressed and converted ${file} to ${outputFilePath}`);
+          console.log(
+            `Compressed and converted ${filePath} to ${outputFilePath}`
+          );
         } catch (err) {
-          console.error(`Error processing ${file}:`, err);
+          console.error(`Error processing ${filePath}:`, err);
         }
       })
     );
