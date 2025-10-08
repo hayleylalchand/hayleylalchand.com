4e34ca32cb1cb1d2e32e0c86d1c3d54ed8ac7074
Ryan Lalchand <70449616+ryanlalchand@users.noreply.github.com>
2025-10-07 20:47:39 -0400

    fix cheerio dependency

--- src/utils/fetchImage.js
+++ src/utils/fetchImage.js
@@ -1,11 +1,11 @@
 import fetch from "node-fetch";
-import cheerio from "cheerio";
+import { load } from "cheerio";
 
 export async function fetchImage(url) {
   try {
     const response = await fetch(url);
     const html = await response.text();
-    const $ = cheerio.load(html);
+    const $ = load(html);
     const imageUrl = $('meta[property="og:image"]').attr("content") || "";
     return imageUrl;
   } catch (error) {
